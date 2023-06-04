1ce0f4a1896968f9fb3538732e1bc4d3e1c2f81c
Leandro Portugal <leandroportugal@outlook.com.br>
2023-06-04 17:45:02 -0300

    create service to return technicals and customers for selection on ticket creation

--- src/app/components/ticket/ticket-create/ticket-create.component.ts
+++ src/app/components/ticket/ticket-create/ticket-create.component.ts
@@ -1,5 +1,13 @@
+// ticket-create.component.ts
+
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Customer } from 'src/app/models/customer';
+import { Technical } from 'src/app/models/technical';
+import { CustomerService } from 'src/app/services/customer.service';
+import { TechnicalService } from 'src/app/services/technical.service';
+import { TicketsService } from 'src/app/services/tickets.service';
 
 @Component({
   selector: 'app-ticket-create',
@@ -7,24 +15,54 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./ticket-create.component.css']
 })
 export class TicketCreateComponent implements OnInit {
-
+  
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  customers: Customer [] = [];
+  technicals: Technical [] = [];
 
-  ngOnInit() {
-    this.firstFormGroup = this.formBuilder.group({
+
+  constructor(private formBuilder: FormBuilder, private router: Router,
+    private customerService: CustomerService, private technicalService: TechnicalService, private ticketService: TicketsService) {}
+
+  ngOnInit(): void {
+    this.router.navigate(['tickets/create']);
+    this.findAllCustomers();
+    this.findAllTechnicals();
+    this.firstFormGroup = this.createFirstFormGroup();
+    this.secondFormGroup = this.createSecondFormGroup();
+  }
+
+  findAllCustomers(): void{
+    this.customerService.findAll().subscribe(response =>{
+      this.customers = response;
+    })
+  }
+
+  findAllTechnicals(): void{
+    this.technicalService.findAll().subscribe(response =>{
+      this.technicals = response;
+    })
+  }
+
+
+
+  private createFirstFormGroup(): FormGroup {
+    return this.formBuilder.group({
       title: ['', Validators.required],
       status: ['', Validators.required],
       priority: ['', Validators.required],
       technical: ['', Validators.required],
       customer: ['', Validators.required]
     });
+  }
 
-    this.secondFormGroup = this.formBuilder.group({
+  private createSecondFormGroup(): FormGroup {
+    return this.formBuilder.group({
       observation: ['', Validators.required]
     });
   }
+
 
 }
